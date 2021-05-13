0589353b8c4cc4f92377d954844e9c1b537fc449
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2021-05-13 15:54:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 video="";
 function preload(){
     video=createVideo('video.mp4');
-    video.hide();
 }
 function setup(){
     canvas=createCanvas(480,380);
     canvas.center();
+    video.hide();
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status != ""){
+        objectDetector.detect(video,gotResult)
+        for (i = 0; i<objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
+            document.getElementById("no_of_objects").innerHTML = "Number fo objects detected are : " + objects.length;
+
+            fill("#FF0000")
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        
+        }
+    }
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelloaded);
@@ -21,3 +36,11 @@ function modelloaded(){
     video.speed(1);
     video.volume(0);
 }
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+}
+objects=[];
+status="";
